694f96469bb03a3819c15cc4a2c5b0e95321e4c3
hideru <jota.16.stigma.world@gmail.com>
2021-08-23 01:38:00 +0900

    first commit

---
+++ AspNovelServer2/Models/Novel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AspNovelServer2.Models
+{
+    /*
+    Columns (index_table)
+    id (Title, Auto Increment)
+    title
+    chapter_num
+    subtitle
+    link
+    last_update (For Title)
+    */
+    public class Novel
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public int ChapterNum { get; set; }
+        public string Subtitle { get; set; }
+        public string Href { get; set; }
+        public string LastUpdate { get; set; }
+    }
+}
