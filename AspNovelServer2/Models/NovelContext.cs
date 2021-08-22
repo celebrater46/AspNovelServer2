ec8e3bb481346355f3b62c339dfdf9a591d4d2ef
hideru <jota.16.stigma.world@gmail.com>
2021-08-23 01:45:58 +0900

    Added Novel.cs and NovelContext.cs

---
+++ AspNovelServer2/Models/NovelContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspNovelServer2.Models
+{
+    public class NovelContext : DbContext
+
+    {
+        public NovelContext(DbContextOptions<NovelContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Novel> Novels { get; set; }
+    }
+}
--- AspNovelServer2/Startup.cs
+++ AspNovelServer2/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using AspNovelServer2.Models;
 
 namespace AspNovelServer2
 {
@@ -27,10 +29,13 @@ namespace AspNovelServer2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "AspNovelServer2", Version = "v1"});
-            });
+            
+            //
+            services.AddDbContext<NovelContext>(opt => opt.UseInMemoryDatabase(NovelIndex));
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo {Title = "AspNovelServer2", Version = "v1"});
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,8 +44,8 @@ namespace AspNovelServer2
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNovelServer2 v1"));
+                // app.UseSwagger();
+                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNovelServer2 v1"));
             }
 
             app.UseHttpsRedirection();
